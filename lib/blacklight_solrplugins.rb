cc8ebf5c916b5476cb8bb5b05fef6cc49560e233
Jeff Chiu <jeffchiu@upenn.edu>
2016-09-08 15:39:45 -0400

    autoload our SearchState class to avoid dependency problem in Blacklight

--- lib/blacklight_solrplugins.rb
+++ lib/blacklight_solrplugins.rb
@@ -4,9 +4,13 @@ require "blacklight"
 require "blacklight_solrplugins/engine"
 require "blacklight_solrplugins/indexer"
 require "blacklight_solrplugins/routes/x_browsable"
-require "blacklight_solrplugins/search_state"
 require "blacklight_solrplugins/util"
 require "blacklight_monkey_patch"
 
 module BlacklightSolrplugins
+  # SearchState MUST be autoloaded; if you eager load it, you'll get
+  # an error about Blacklight::Facet not found b/c it's a controller
+  # concern that's not loaded by Rails yet. This is fixed in
+  # commit c38073a in the Blacklight repo.
+  autoload :SearchState, 'blacklight_solrplugins/search_state'
 end
