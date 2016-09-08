110729bae2ae05daa19588c64a18cd0b7150330e
Jeff Chiu <jeffchiu@upenn.edu>
2016-09-08 16:21:32 -0400

    more resilient string handling

--- app/helpers/blacklight_solrplugins/application_helper.rb
+++ app/helpers/blacklight_solrplugins/application_helper.rb
@@ -2,13 +2,13 @@ module BlacklightSolrplugins
   module ApplicationHelper
 
     # item can be a string or a FacetItem, as this used for both rendering
-    # facets sidevar AND the value from a URL facet param.
+    # the value from a URL facet param AND the facets sidebar.
     def facet_display_value(field, item)
       facet_config = facet_configuration_for_field(field)
       if facet_config[:xfacet]
         if item.is_a?(String)
           filing, prefix = item.split("|")
-          return prefix + filing
+          return prefix.to_s + filing.to_s
         else
           _self = item.payload[:self]
           if _self
