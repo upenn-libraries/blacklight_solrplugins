f82abe34e975e9fb8d41745e0d95956ae2e1a9a1
Jeff Chiu <jeffchiu@upenn.edu>
2016-09-19 16:39:08 -0400

    fill out HTML UI for xbrowse, including pagination and per_page widget; override #search_action_path to generate correct links back to xbrowse urls

--- app/controllers/concerns/blacklight_solrplugins/x_browse.rb
+++ app/controllers/concerns/blacklight_solrplugins/x_browse.rb
@@ -5,15 +5,46 @@ require 'json'
 module BlacklightSolrplugins::XBrowse
   extend ActiveSupport::Concern
 
+  # override from Blacklight::Controller.
+  # Since this gem effectively extends the notion of a "search action"
+  # to include xbrowse pages, we need this method to return the right links
+  # for facets sidebar, query constraints panel, and maybe other places.
+  def search_action_path(*args)
+    if params[:action] == 'xbrowse'
+      url_for(*args)
+    else
+      super(*args)
+    end
+  end
+
+  # action
   def xbrowse
     target = params[:target]
+    ref = params[:ref] # reference point, used for paging
+    dir = params[:dir]
+    per_page = params.fetch(:per_page, blacklight_config.default_per_page).to_i
+
+    offset = 0
+    if dir == 'back'
+      offset = per_page
+    elsif dir == 'forward'
+      offset = -1
+    end
+
+    facet_target = ref || target || ''
+
     @facet = blacklight_config.facet_fields[params[:id]]
     (@response, @document_list) = search_results(params.merge(:rows => 0)) do |search_builder|
-      search_builder.merge({
-        "f.#{@facet.field}.facet.target": JSON.dump(target),
-        "f.#{@facet.field}.facet.sort": 'index',
-        "f.#{@facet.field}.facet.offset": 0,
-        "f.#{@facet.field}.facet.limit": 25 })
+      additional_params = {
+          "f.#{@facet.field}.facet.target": JSON.dump(facet_target),
+          "f.#{@facet.field}.facet.sort": 'index',
+          "f.#{@facet.field}.facet.offset": offset,
+          "f.#{@facet.field}.facet.limit": per_page }
+      if dir
+        # TODO: this is throwing things off
+        #additional_params["f.#{@facet.field}.facet.target.strict"] = true
+      end
+      search_builder.merge(additional_params)
     end
     @display_facet = @response.aggregations[@facet.key]
     respond_to do |format|
--- app/helpers/blacklight_solrplugins/application_helper.rb
+++ app/helpers/blacklight_solrplugins/application_helper.rb
@@ -1,6 +1,7 @@
 module BlacklightSolrplugins
   module ApplicationHelper
 
+    # override from Blacklight::FacetsHelperBehavior.
     # item can be a string or a FacetItem, as this used for both rendering
     # the value from a URL facet param AND the facets sidebar.
     def facet_display_value(field, item)
@@ -24,9 +25,35 @@ module BlacklightSolrplugins
       "#{count} record#{count > 1 ? 's' : ''}"
     end
 
-    def xbrowse_filter_on_facet_path(facet_value)
-      # TODO: generate a correct URL that filters on this facet value
-      xbrowse_catalog_path(:target => params[:target])
+    # returns a URL that filters on this facet value
+    def path_for_xfacet(xfacet_field, facet_value)
+      # NOTE that we use the facet_for_filtering field, NOT the xfacet field,
+      # when adding a new query constraint
+      path_for_facet(xfacet_field['facet_for_filtering'], facet_value)
+    end
+
+    def xbrowse_show_previous_link?(facet)
+      # TODO
+      true
+    end
+
+    def xbrowse_show_next_link?(facet)
+      # TODO
+      true
+    end
+
+    def xbrowse_previous_link(facet)
+      first = facet.items.first
+      if first
+        url_for(search_state.params_for_search(ref: first.value, dir: "back"))
+      end
+    end
+
+    def xbrowse_next_link(facet)
+      last = facet.items.last
+      if last
+        url_for(search_state.params_for_search(ref: last.value, dir: "forward"))
+      end
     end
 
   end
